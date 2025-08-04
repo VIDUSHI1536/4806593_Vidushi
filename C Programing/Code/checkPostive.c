92af166399f2eacb1e0b2a4bb776dc97ce6b6129
Vidushi Jaiswal <vidushij078@gmail.com>
2025-08-04 12:31:34 +0530

    Coding

---
+++ C Programing/Code/checkPostive.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    if(n<0){
+        printf("Negative number!");
+    }
+    else{
+        printf("Postive number");
+    }
+    return 0;
+}
